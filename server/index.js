56a4563ef67c49af6221921049a87a59609ff9e6
Gbaja <gbajaf@yahoo.co.uk>
2018-05-03 16:32:10 +0100

    changed build path

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(routes);
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve("/app/build/index.html"));
   });
