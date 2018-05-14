db5bba50b6a11371947f346ab6734ea71941d0ab
Gbaja <gbajaf@yahoo.co.uk>
2018-05-14 12:02:11 +0100

    Included reset for mentee registrations relates to #10

--- server/routes/forgot_password.js
+++ server/routes/forgot_password.js
@@ -3,16 +3,33 @@ const models = require("../models");
 const forgotPasswordEmail = require("../utils/email_templates/forgot_password_email");
 
 exports.post = (req, res) => {
+  const token = crypto.randomBytes(20).toString("base64");
   models.MentorRegistrations.findOne({ where: { email: req.body.email } }).then(
     mentor => {
       if (!mentor) {
-        res.status(422).send("User does not exists, please create an account.");
+        models.MenteeRegistrations.findOne({
+          where: { email: req.body.email }
+        }).then(mentee => {
+          if (!mentee) {
+            res
+              .status(422)
+              .send("User does not exists, please create an account.");
+          } else {
+            models.MenteeRegistrations.update(
+              {
+                passwordResetToken: token
+              },
+              { returning: true, where: { email: req.body.email } }
+            ).then(([rowsUpdate, [updatedProfile]]) => {
+              forgotPasswordEmail(updatedProfile, req);
+              res.send(updatedProfile);
+            });
+          }
+        });
       } else {
-        const token = crypto.randomBytes(20).toString("base64");
         models.MentorRegistrations.update(
           {
-            passwordResetToken: token,
-            passwordResetTokenExpires: Date.now() + 3600000
+            passwordResetToken: token
           },
           { returning: true, where: { email: req.body.email } }
         ).then(([rowsUpdate, [updatedProfile]]) => {
--- server/routes/reset_password.js
+++ server/routes/reset_password.js
@@ -6,13 +6,36 @@ exports.post = (req, res) => {
   console.log({ password: req.body.password, token: req.query.token });
   models.MentorRegistrations.findOne({
     where: { passwordResetToken: req.query.token }
-  }).then(user => {
-    if (!user) {
-      res.status(422).send("Invalid password token.");
+  }).then(mentor => {
+    if (!mentor) {
+      models.MenteeRegistrations.findOne({
+        where: { passwordResetToken: req.query.token }
+      }).then(mentee => {
+        if (!mentee) {
+          res.status(422).send("Invalid password token.");
+        } else {
+          hashPassword(req.body.password).then(password => {
+            models.MenteeRegistrations.update(
+              {
+                password: password
+              },
+              {
+                returning: true,
+                where: { passwordResetToken: req.query.token }
+              }
+            ).then(([rowsUpdate, [updatedProfile]]) => {
+              console.log("UPDATED PROFILE: ", updatedProfile);
+              resetPasswordEmail(updatedProfile, req);
+              res.send({
+                message: "Your password has been updated, please sign in! ",
+                data: updatedProfile
+              });
+            });
+          });
+        }
+      });
     } else {
       hashPassword(req.body.password).then(password => {
-        console.log("THEN PASSWORD: ", password);
-        console.log("USERL ", user);
         models.MentorRegistrations.update(
           {
             password: password
