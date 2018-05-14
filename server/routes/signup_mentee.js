de168db3816457bf2a84faa62e821b296b3f3765
Gbaja <gbajaf@yahoo.co.uk>
2018-05-14 12:15:57 +0100

    Sign up for mentor and mentee now checks if user exists in both tables

--- server/routes/signup_mentee.js
+++ server/routes/signup_mentee.js
@@ -10,29 +10,41 @@ exports.post = (req, res) => {
     password,
     confirmDetails
   } = req.body;
-  return models.MenteeRegistrations.findOne({
+  models.MenteeRegistrations.findOne({
     where: { email: email.toLowerCase() }
-  }).then(existingUser => {
-    if (existingUser) {
+  }).then(existingMentee => {
+    if (existingMentee) {
       return res
         .status(422)
         .send(
-          "This email address has already been used to create an account with Young&giving, please try logging in."
+          "This email address has already been used to create a mentee account with Young&giving, please try logging in."
         );
     } else {
-      return models.MenteeRegistrations.create({
-        firstName,
-        lastName,
-        accountType,
-        email,
-        password,
-        confirmDetails
-      }).then(data => {
-        req.session.user_id = data.dataValues.id;
-        console.log("SIGN UP COOKIE: ", req.session);
-        console.log("sign up data : ", data.dataValues.id);
-        signupTemplate(data.dataValues);
-        res.send(data.dataValues);
+      models.MentorRegistrations.findOne({
+        where: { email: email.toLowerCase() }
+      }).then(existingMentor => {
+        if (existingMentor) {
+          return res
+            .status(422)
+            .send(
+              "This email address has already been used to create a mentor with Young&giving, please try logging in."
+            );
+        } else {
+          models.MenteeRegistrations.create({
+            firstName,
+            lastName,
+            accountType,
+            email,
+            password,
+            confirmDetails
+          }).then(data => {
+            req.session.user_id = data.dataValues.id;
+            console.log("SIGN UP COOKIE: ", req.session);
+            console.log("sign up data : ", data.dataValues.id);
+            signupTemplate(data.dataValues);
+            res.send(data.dataValues);
+          });
+        }
       });
     }
   });
